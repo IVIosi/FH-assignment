d04f044f4e013db6786cf37948ac828be70160bd
Mostafa <mos.sameti@gmail.com>
2021-01-07 00:13:07 +0330

    add theme configs

---
+++ src/configs/theme-configs.ts
@@ -0,0 +1,16 @@
+const themeConfigs = {
+  palette: {
+    primary: '#0077ff',
+    secondary: '#4398fa',
+    text: {
+      primary: '#2a333d',
+      secondary: '#a3acb8',
+    },
+    background: {
+      default: '#ffffff',
+      box: '#fefeff',
+    },
+  },
+};
+
+export default themeConfigs;
