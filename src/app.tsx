8ae64ae245a76544ddf7d03df4cd6457264e27fd
Mostafa <mos.sameti@gmail.com>
2021-01-07 18:06:46 +0330

    add header structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,23 @@
-import React, { FC } from 'react';
+import { FC } from 'react';
 import { Provider } from 'react-redux';
-import { ThemeProvider } from '@emotion/react';
+import { ThemeProvider, Global } from '@emotion/react';
 
-import { storeConfigs, themeConfigs } from './configs';
-import './configs/i18n-configs';
+import globalStyles from '@assets/styles/global';
+import { storeConfigs, themeConfigs } from '@configs/index';
+import '@configs/i18n-configs';
+import Header from '@components/header';
+import Filters from '@components/filters';
 
 const store = storeConfigs();
 
 const App: FC = () => {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={themeConfigs}>FindHotel</ThemeProvider>
+      <ThemeProvider theme={themeConfigs}>
+        <Global styles={globalStyles} />
+        <Header />
+        <Filters />
+      </ThemeProvider>
     </Provider>
   );
 };
--- src/assets/types.d.ts
+++ src/assets/types.d.ts
@@ -1,4 +1,20 @@
-declare module '*.eot';
-declare module '*.ttf';
-declare module '*.woff';
-declare module '*.woff2';
+import '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    palette: {
+      primary: string;
+      secondary: string;
+      text: {
+        primary: string;
+        secondary: string;
+      };
+      background: {
+        default: string;
+        box: string;
+      };
+    };
+    spacing: (step: number) => number;
+    fade: (color: string, alpha: number) => string;
+  }
+}
