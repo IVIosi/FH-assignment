9333fc48e55e7eb97ea7f0ce2d80aa2cf43d9ea0
Mostafa <mos.sameti@gmail.com>
2021-01-07 21:36:17 +0330

    update filters component styles

--- src/components/filters/index.tsx
+++ src/components/filters/index.tsx
@@ -1,23 +1,33 @@
-import { FC } from 'react';
+import { FC, createElement } from 'react';
+import BreakfastIcon from '@assets/roomfilter/breakfast.svg';
+import PaylaterIcon from '@assets/roomfilter/paylater.svg';
+import PayhotelIcon from '@assets/roomfilter/payhotel.svg';
+import NonsmokingIcon from '@assets/roomfilter/non-smoking.svg';
+import WifiIcon from '@assets/roomfilter/wifi.svg';
+
+import classes from './styles';
 
 const Filters: FC = () => {
+  const filterOptions = [
+    { name: 'Breakfast', icon: createElement(BreakfastIcon, null) },
+    { name: 'Pay Later', icon: createElement(PaylaterIcon, null) },
+    { name: 'Pay at Hotel', icon: createElement(PayhotelIcon, null) },
+    { name: 'Non Smoking', icon: createElement(NonsmokingIcon, null) },
+    { name: 'Free WiFi', icon: createElement(WifiIcon, null) },
+  ];
   return (
-    <div
-      css={(theme) => ({
-        height: '96px',
-        borderTop: '1px solid rgba(0, 44, 97, 0.05)',
-        borderBottom: '1px solid rgba(0, 44, 97, 0.05)',
-      })}
-    >
-      <div
-        css={(theme) => ({
-          fontSize: '10px',
-          color: theme.palette.text.secondary,
-          textAlign: 'center',
+    <div css={classes.filterSection}>
+      <div css={classes.sectionTitle}>Filters</div>
+      <ol css={classes.optionCarousel}>
+        {filterOptions.map((option) => {
+          return (
+            <li key={option.name} css={classes.filterOption}>
+              <div css={classes.filterIcon}>{option.icon}</div>
+              <span css={classes.filterName}>{option.name}</span>
+            </li>
+          );
         })}
-      >
-        Filters
-      </div>
+      </ol>
     </div>
   );
 };
---
+++ src/components/filters/styles.ts
@@ -0,0 +1,55 @@
+import { Interpolation, Theme } from '@emotion/react';
+
+const styles: { [className: string]: Interpolation<Theme> } = {
+  filterSection: {
+    height: '96px',
+    borderTop: '1px solid rgba(0, 44, 97, 0.05)',
+    borderBottom: '1px solid rgba(0, 44, 97, 0.05)',
+    backgroundColor: 'rgba(254, 254, 255, 0.95)',
+  },
+  sectionTitle: (theme) => ({
+    fontSize: '10px',
+    color: theme.palette.text.secondary,
+    textAlign: 'center',
+  }),
+  optionCarousel: {
+    display: 'flex',
+    listStyle: 'none',
+    margin: '10px 0',
+    padding: '0',
+    overflow: 'scroll',
+  },
+  filterOption: (theme) => ({
+    position: 'relative',
+    height: theme.spacing(8),
+    margin: `0 ${theme.spacing(2)}px`,
+    cursor: 'pointer',
+    color: theme.palette.primary,
+    backgroundColor: theme.palette.background.default,
+    '&:hover': {
+      color: theme.palette.background.default,
+      '& div': {
+        backgroundColor: theme.palette.secondary,
+        border: `2px solid ${theme.palette.primary}`,
+      },
+    },
+  }),
+  filterIcon: {
+    borderRadius: '50%',
+    border: `2px solid #bed2e9`,
+    width: '40px',
+    height: '40px',
+  },
+  filterName: (theme) => ({
+    position: 'absolute',
+    fontSize: '10px',
+    lineHeight: '20px',
+    right: '50%',
+    transform: 'translateX(50%)',
+    width: theme.spacing(8),
+    textAlign: 'center',
+    color: theme.palette.primary,
+  }),
+};
+
+export default styles;
